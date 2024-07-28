fefcdd70d5ac8ab5b0043fa5304da6b48d375e78
Carlos Gleison Caetano de Oliveira <gleison.resident@hotmail.com>
2024-07-28 16:08:18 -0300

    Conceitus ERP

--- my-financeiro/src/app/app.routes.ts
+++ my-financeiro/src/app/app.routes.ts
@@ -14,5 +14,17 @@ export const routes: Routes = [
     {
         path: 'dashboard',
         loadChildren: () => import('./pages/dashboard/dashboard.module').then(m => m.DashboardModule)
+    },
+    {
+        path: 'sistema',
+        loadChildren: () => import('./pages/sistema/sistema.module').then(m => m.SistemaModule)
+    },
+    {
+        path: 'categoria',
+        loadChildren: () => import('./pages/categoria/categoria.module').then(m => m.CategoriaModule)
+    },
+    {
+        path: 'despesa',
+        loadChildren: () => import('./pages/despesa/despesa.module').then(m => m.DespesaModule)
     }
 ];
